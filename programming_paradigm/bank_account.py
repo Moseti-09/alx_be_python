853fb1cfe91923e08776f3ebce236b545371435b
Cliff Moseti <cmbichanga@gmail.com>
2025-11-27 18:01:30 +0300

    Implement BankAccount class for OOP fundamentals

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,15 +1,23 @@
 class BankAccount:
-    def __init__(self, initial_balance=0):
-        self.account_balance = initial_balance
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance
 
+    
     def deposit(self, amount):
-        self.account_balance += amount
-
+        if amount > 0:
+            self.account_balance += amount
+            return True
+        return False
+    
     def withdraw(self, amount):
-        if self.account_balance >= amount:
+        if 0 < amount <= self.account_balance:
             self.account_balance -= amount
             return True
         return False
+       
+        
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
+        
+        
